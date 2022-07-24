64f8e053e0051874bd490d444163152417e1b24c
Gregory Boyd <gboyd69@yahoo.com>
2022-07-24 17:57:00 -0400

    added date formatting functions

--- resources/scripts/dates.js
+++ resources/scripts/dates.js
@@ -1,4 +1,50 @@
 //Retirement Dates => calculate total federal service time
-let enterOnDate = document.querySelector("#enterOnDate").value;
-let retirementDate = document.querySelector("#retirementDate").value;
-console.log(enterOnDate);
+let enterOnDate = document.querySelector("#enterOnDate");
+let retirementDate = document.querySelector("#retirementDate");
+let milStartDate = document.querySelector("#milStartDate");
+let milEndDate = document.querySelector("#milEndDate");
+
+let startDate;
+let retireDate;
+let retirementDays;
+
+enterOnDate.addEventListener("change", () => {
+  startDate = formatDate(enterOnDate.value);
+});
+
+retirementDate.addEventListener("change", () => {
+  retireDate = formatDate(retirementDate.value);
+  retirementDays = serviceTime(startDate, retireDate);
+  console.log(retirementDays);
+});
+
+milStartDate.addEventListener("change", () => {
+  let milStart = formatDate(milStartDate.value);
+  console.log(milStart);
+  //Add other code:
+});
+
+milEndDate.addEventListener("change", () => {
+  let milEnd = formatDate(milEndDate.value);
+  console.log(milEnd);
+  //add other code:
+});
+
+//Calculate the difference between two dates:
+const serviceTime = (date1, date2) => {
+  const startDate = new Date(date1);
+  const endDate = new Date(date2);
+
+  const timeDifference = endDate.getTime() - startDate.getTime();
+  const daysDifference = timeDifference / (1000 * 60 * 60 * 24) + 1;
+  return daysDifference;
+};
+
+function formatDate(inputDate) {
+  //Must use UTC values to fix being off by one day:
+  const workingDate = new Date(inputDate);
+  const day = workingDate.getUTCDate();
+  const month = workingDate.getUTCMonth() + 1;
+  const year = workingDate.getUTCFullYear();
+  return month + "/" + day + "/" + year;
+}
--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,34 +1,27 @@
-// get Military Buyback value:
+// Military Buyback value to display/hide Military buyback dates:
 let milBuyback = document.querySelector("#milBuyback");
 let milDateElements = document.querySelector(".military-dates");
-let firstName = document.querySelector("#firstName");
-let buybackAnswer = milBuyback.value;
-
-console.log(buybackAnswer);
 
 //Show Military dates if the user selects buyback option "yes"
-milBuyback.addEventListener("change", function (event) {
-  let selected = event.target.value;
-  console.log(selected);
-  if (selected) {
+milBuyback.addEventListener("change", (event) => {
+  let boughtBack = event.target.value;
+  console.log(boughtBack);
+  if (boughtBack) {
     milDateElements.style.display = "block";
   } else {
     milDateElements.style.display = "none";
   }
 });
 
-firstName.addEventListener("input", () => {
-  console.log(firstName.value);
-});
-
 //Create array of all the input elements, add evetlistener and style when focused:
 const inputElements = document.getElementsByTagName("input");
 for (let i = 0; i < inputElements.length; i++) {
   inputElements[i].addEventListener("focus", (e) => {
-    e.target.style.backgroundColor = "lightgray";
+    e.target.style.backgroundColor = "hsla(0,5%,81%,0.6)";
   });
   //return background color to original state:
   inputElements[i].addEventListener("focusout", (e) => {
     e.target.style.backgroundColor = "white";
   });
 }
+
