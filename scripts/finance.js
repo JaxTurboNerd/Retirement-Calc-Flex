82905ac8aaba41ffcfbb1b1678f8abed10871b6b
Gregory Boyd <gboyd69@yahoo.com>
2022-08-28 14:33:53 -0400

    added formula for how long money will last. updated % input layout

--- scripts/finance.js
+++ scripts/finance.js
@@ -1,58 +1,51 @@
 import { googleAPIKey, yahooAPIKey } from "../config.js";
 
-// Just-validate configuration settings:
-// const validation = new JustValidate("#futureValue-form", {
-//   errorFieldCssClass: "is-invalid",
-//   successFieldCssClass: "is-valid",
-//   errorLabelCssClass: "is-label-invalid",
-//   focusInvalidField: "true",
-//   lockForm: "true",
-//   tooltip: {
-//     position: "right",
-//   },
-// });
-
 //connect to user input
 let tspBalance = document.querySelector("#tspBalance");
 let ppContributions = document.querySelector("#ppContributions");
 let returnRate = document.querySelector("#returnRate");
 let futureYears = document.querySelector("#futureYears");
-let calculateButton = document.querySelector(".calculate__btn");
+let tspButton = document.querySelector(".tsp__btn");
 let savingsBalance = document.querySelector("#savingsBalance");
 let withdrawlAmount = document.querySelector("#withdrawl");
 let frequency = document.querySelector("#frequency");
 let investmentReturn = document.querySelector("#investmentReturn");
 let inflation = document.querySelector("#inflation");
 let taxRate = document.querySelector("#taxRate");
+let savingsButton = document.querySelector(".savings__btn");
 
 //Variable declarations:
 let foundError = false;
 
-//Number(Currency) formatting:
+//Currency formatting:
 let usCurrency = Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
 
 //Input validation functions:
-function checkValidNumber(input) {
+function isValidNumber(input) {
   //remove the commas from the input string
   let newValue = parseFloat(input.value.replace(/,/g, ""));
   if (isNaN(newValue) || newValue < 0) {
     foundError = true;
     showError(input);
+    return false;
   } else {
     resetClass(input);
     foundError = false;
+    return true;
   }
 }
 
+//displayss a <small> error tag </small> if a validation error exists:
 function showError(input) {
   const dollarFlex = input.parentElement;
   const formControl = dollarFlex.parentElement;
   formControl.className = "form-control error";
 }
 
+//resets/hides the error small tag
 function resetClass(input) {
   const dollarFlex = input.parentElement;
   const formControl = dollarFlex.parentElement;
@@ -61,9 +54,10 @@ function resetClass(input) {
 
 //Calculate the future value of the user's TSP balance:
 function calculateTSPValue() {
+  //the replace function removes the commas from the user input
   const tspValue = parseFloat(tspBalance.value.replace(/,/g, ""));
   const contributionValue = parseFloat(ppContributions.value.replace(/,/g, ""));
-  const yearlyContribution = contributionValue * 26;
+  const yearlyContribution = contributionValue * 26; //26 pay periods per year
   const annualizedRate = 1 + parseFloat(returnRate.value) * 0.01;
   const years = parseInt(futureYears.value);
 
@@ -82,6 +76,31 @@ function calculateTSPValue() {
   )}`;
 }
 
+//This calculates how long the user's money will last
+function calculateInvestmentDuration() {
+  // m = interest rate
+  // p = principal
+  // w = withdrawl amount
+  // n = periods
+
+  // n = -(Log[1 - (m p)/w]/Log[1 + m])
+  const balance = parseFloat(savingsBalance.value.replace(/,/g, ""));
+  const withdrawlSum = parseFloat(withdrawlAmount.value.replace(/,/g, ""));
+  const interestRate = parseFloat(investmentReturn.value) * 0.01;
+  const numerator = Math.log(1 - (interestRate * balance) / withdrawlSum);
+  const denominator = Math.log(1 + interestRate);
+
+  //Need to check for zeros entered by user...will result in NaN
+  //code here:
+
+  const periods = -(numerator / denominator);
+  console.log(
+    `balance: ${balance} withdrawl: ${withdrawlSum} rate: ${interestRate}`
+  );
+  console.log(periods);
+  return periods;
+}
+
 //Event Listeners:
 tspBalance.addEventListener("input", (event) => {
   //provides input mask with comma formatting
@@ -92,6 +111,18 @@ tspBalance.addEventListener("input", (event) => {
 
 ppContributions.addEventListener("input", (event) => {
   //provides input mask with comma formatting
+  event.target.value = (
+    parseInt(event.target.value.replace(/[^\d]+/gi, "")) || 0
+  ).toLocaleString("en-US");
+});
+
+returnRate.addEventListener("input", (event) => {
+  //validates only numbers and decimals
+  event.target.value = event.target.value.replace(/[^\d.]/gi, "");
+});
+
+futureYears.addEventListener("input", (event) => {
+  //make sure there will always be a value in the input field:
   event.target.value = (
     parseInt(event.target.value.replace(/[^\d]+/gi, "")) || 0
   ).toLocaleString("en-US");
@@ -111,21 +142,57 @@ withdrawlAmount.addEventListener("input", (event) => {
   ).toLocaleString("en-US");
 });
 
-calculateButton.addEventListener("click", () => {
+investmentReturn.addEventListener("input", (event) => {
+  //validates only numbers and decimals
+  event.target.value = event.target.value.replace(/[^\d.]/gi, "");
+});
+
+inflation.addEventListener("input", (event) => {
+  //validates only numbers and decimals
+  event.target.value = event.target.value.replace(/[^\d.]/gi, "");
+});
+
+taxRate.addEventListener("input", (event) => {
+  //validates only numbers and decimals
+  event.target.value = event.target.value.replace(/[^\d.]/gi, "");
+});
+
+tspButton.addEventListener("click", () => {
   //run through numerical validations for each input field
-  checkValidNumber(tspBalance);
-  if (!foundError) {
-    checkValidNumber(ppContributions);
-  }
-  if (!foundError) {
-    checkValidNumber(returnRate);
-  }
-  if (!foundError) {
-    checkValidNumber(futureYears);
-  }
-  if (!foundError) {
-    //finally calculate the total result if no errors
+  const inputFields = [tspBalance, ppContributions, returnRate, futureYears];
+  //loop through the input element array.  this loop breaks out with a falsy value
+  let foundNoErrors = inputFields.every((input) => {
+    if (!isValidNumber(input)) {
+      return false;
+    }
+    return true;
+  });
+
+  if (foundNoErrors) {
     calculateTSPValue();
+  }
+});
+
+savingsButton.addEventListener("click", () => {
+  //Validations for input fields:
+  const inputFields = [
+    savingsBalance,
+    withdrawlAmount,
+    investmentReturn,
+    inflation,
+    taxRate,
+  ];
+
+  //loop through input fields and validate user input:
+  let foundNoErrors = inputFields.every((input) => {
+    if (!isValidNumber(input)) {
+      return false;
+    }
+    return true;
+  });
+
+  if (foundNoErrors) {
+    calculateInvestmentDuration();
   }
 });
 
